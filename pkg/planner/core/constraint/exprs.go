78bed1ac46dfd3ca2cbd98e7f3f1c52c50f6e252
Weizhen Wang <wangweizhen@pingcap.com>
2025-01-26 12:05:42 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/constraint/exprs.go
+++ pkg/planner/core/constraint/exprs.go
@@ -44,13 +44,13 @@ func DeleteTrueExprs(p base.LogicalPlan, conds []expression.Expression) []expres
 
 // IsConstFalse is used to check whether the expression is a constant false expression.
 func IsConstFalse(ctx expression.BuildContext, expr expression.Expression) bool {
-	if expression.MaybeOverOptimized4PlanCache(ctx, []expression.Expression{expr}) {
-		ctx.SetSkipPlanCache("some parameters may be overwritten when constant propagation")
-	}
 	if e, ok := expr.(*expression.ScalarFunction); ok {
 		switch e.FuncName.L {
 		case ast.LT, ast.LE, ast.GT, ast.GE, ast.EQ, ast.NE:
 			if constExpr, ok := e.GetArgs()[1].(*expression.Constant); ok && constExpr.Value.IsNull() && constExpr.DeferredExpr == nil {
+				if expression.MaybeOverOptimized4PlanCache(ctx, []expression.Expression{expr}) {
+					ctx.SetSkipPlanCache("some parameters may be overwritten when constant propagation")
+				}
 				return true
 			}
 		}
